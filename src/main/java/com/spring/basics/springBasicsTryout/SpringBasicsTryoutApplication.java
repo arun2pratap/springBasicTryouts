e28f5d0624d98965bfd59502e6af16bf586702cf
Neo <arun2pratap@gmail.com>
2018-08-03 06:33:28 +0530

    first commit

---
+++ src/main/java/com/spring/basics/springBasicsTryout/SpringBasicsTryoutApplication.java
@@ -0,0 +1,12 @@
+package com.spring.basics.springBasicsTryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsTryoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsTryoutApplication.class, args);
+	}
+}
